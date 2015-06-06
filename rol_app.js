2c93890cadc3fb5787ebfe4af5b7c87ec9a87184
Jorge Rojas <jorgee@Jorges-MacBook-Pro.local>
2015-06-05 20:59:17 -0700

    rol_app.js modified port

--- rol_app.js
+++ rol_app.js
@@ -5,12 +5,13 @@ app.use(express.static(__dirname + '/'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
 	res.render('rol_spark.html');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
 
 	var host = server.address().address;
 	var port = server.address().port;
