002367f97757881f03d2dea8482bebefa214b28e
Jorge Rojas <jorgee@Jorges-MacBook-Pro.local>
2015-06-05 19:15:39 -0700

    Working ino, html, and js files

--- rol_app.js
+++ rol_app.js
@@ -7,7 +7,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
-	res.render('RhythmOfLove.html');
+	res.render('rol_spark.html');
 });
 
 var server = app.listen(3000, function () {
